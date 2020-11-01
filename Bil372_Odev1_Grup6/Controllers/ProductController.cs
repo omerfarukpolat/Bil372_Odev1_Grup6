3c6ec2a207559f07448bccf39fb77f241c747105
abozgungordu <abozgungordu@gmail.com>
2020-11-01 23:50:03 +0300

    Some controllers and pages are inserted.

---
+++ Bil372_Odev1_Grup6/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bil372_Odev1_Grup6.Controllers
+{
+  
+        public class ProductController : Controller
+        {
+            // GET: Product/Create
+            public ActionResult Create()
+            {
+
+
+                return View();
+            }
+
+            // POST: Product/Create
+            [HttpPost]
+            public ActionResult Create(string pcode, string pname, string pshortname, string pcategory)
+            {
+
+
+                return View();
+
+            }
+
+            // GET: Product/Read
+            public ActionResult Read()
+            {
+                return View();
+            }
+
+            // GET: Product/Update
+            public ActionResult Update()
+            {
+                return View();
+            }
+
+
+            // GET: Product/Delete
+            public ActionResult Delete()
+            {
+                return View();
+            }
+
+
+        }
+    }
