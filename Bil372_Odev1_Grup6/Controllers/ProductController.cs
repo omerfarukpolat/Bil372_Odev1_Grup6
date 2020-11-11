24571999fb84b9097e179bd1f204fec7dc03484c
safa.ergnn <safa.ergnn@gmail.com>
2020-11-11 17:01:50 +0300

    Update ProductController.cs
    Create Uptade Delete functions are fixed.

--- Bil372_Odev1_Grup6/Controllers/ProductController.cs
+++ Bil372_Odev1_Grup6/Controllers/ProductController.cs
@@ -23,10 +23,33 @@ namespace Bil372_Odev1_Grup6.Controllers
             [HttpPost]
             public ActionResult Create(string pcode, string pname, string pshortname, string pparentcode, string pcategory)
             {
-              db.insertProduct(pcode, pname, pshortname, Int32.Parse(pparentcode) , true, pcategory, true);              
-                return View();
-
+            List<PRODUCT> p = db.getProduct();
+            if (p.Count == 0)
+            {
+                db.insertProduct(pcode, pname, pshortname, "NULL", false, pcategory, true);
             }
+            else
+            {
+                Boolean check = false;
+                foreach (var product in p)
+                {
+
+                    if (product.M_CATEGORY.Equals(pparentcode))
+                    {
+                        db.insertProduct(pcode, pname, pshortname, product.M_CODE, false, pcategory, true);
+                        db.updateProduct(product.M_SYSCODE, product.M_CODE, product.M_NAME, product.M_SHORTNAME, product.M_PARENTCODE, true, product.M_CATEGORY, true);
+                        check = true;
+                    }
+
+                }
+                if (!check)
+                {
+                    db.insertProduct(pcode, pname, pshortname, "NULL", false, pcategory, true);
+                }
+            }
+            return View();
+
+        }
 
             // GET: Product/Read
             public ActionResult Read()
@@ -46,7 +69,17 @@ namespace Bil372_Odev1_Grup6.Controllers
             [HttpPost]
             public ActionResult Update(string scode,string pcode, string pname, string pshortname, string pparentcode, string pcategory)
             {
-                db.updateProduct(Int32.Parse(scode), pcode, pname, pshortname, Int32.Parse(pparentcode) , true, pcategory, true);
+            List<PRODUCT> products = db.getProduct();
+            bool isAbstract = false;
+
+            foreach(var product in products)
+            {
+                if (Int32.Parse(scode) == product.M_SYSCODE)
+                {
+                    isAbstract = product.M_ABSTRACT;
+                }
+            }
+                 db.updateProduct(Int32.Parse(scode), pcode, pname, pshortname, pparentcode , isAbstract, pcategory, true);
                 return View();
 
             }
@@ -55,13 +88,44 @@ namespace Bil372_Odev1_Grup6.Controllers
             // GET: Product/Delete
             public ActionResult Delete(string scode, string value)
                 {
-                
-                if(scode != null)
-                    db.deleteFromProduct(Int32.Parse(scode),Int32.Parse(value));
-                List<PRODUCT> products = db.getProduct();
-                    return View(products);
+
+            List<PRODUCT> products = db.getProduct();
+            if (scode != null)
+            {
+                db.deleteFromProduct(Int32.Parse(scode), Int32.Parse(value));
+                 
+                 foreach (var product in products)
+                {
+                    bool check = false;
+                    if (Int32.Parse(scode) == product.M_SYSCODE)
+                    {
+                        string mcode = product.M_CODE;
+                        foreach (var product2 in products)
+                        {
+                            if (product2.M_PARENTCODE.Equals(mcode))
+                            {
+                                check = true;
+                            }
+                        }
+                        if (!check)
+                        {
+                            foreach (var parent in products)
+                            {
+                                if (product.M_PARENTCODE.Equals(parent.M_CODE))
+                                {
+                                    db.updateProduct(parent.M_SYSCODE, parent.M_CODE, parent.M_NAME, parent.M_SHORTNAME, parent.M_PARENTCODE, false, parent.M_CATEGORY, true);
+                                }
+                            }
+                        }
+                    }
+                } 
+            }
+            return View(products);
                 }
-       
+        
+        
+
+
 
 
     }
