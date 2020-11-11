e5014bfc4bd2d00c41af62fcfe65c505b358932a
safa.ergnn <safa.ergnn@gmail.com>
2020-11-11 17:03:13 +0300

    LINKINBRANDORGS models are created.

---
+++ Bil372_Odev1_Grup6/Models/LINKINGBRANDORGS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bil372_Odev1_Grup6.Models
+{
+    public class LINKINGBRANDORGS
+    {
+        public string BRAND_NAME { get; set; }
+
+        public string ORG_NAME { get; set; }
+
+        public float INNN { get; set; }
+
+        public float OUTTTT { get; set; }
+
+        public float QUANTITY { get; set; }
+    }
+}
