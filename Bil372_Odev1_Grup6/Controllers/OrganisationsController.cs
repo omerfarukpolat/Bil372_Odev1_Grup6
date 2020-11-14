8ad7d6547ca2170a68a5bf0151d585762778b53d
safa.ergnn <safa.ergnn@gmail.com>
2020-11-15 01:01:14 +0300

    Create update delete functions are fixed

--- Bil372_Odev1_Grup6/Controllers/OrganisationsController.cs
+++ Bil372_Odev1_Grup6/Controllers/OrganisationsController.cs
@@ -9,8 +9,8 @@ namespace Bil372_Odev1_Grup6.Controllers
 {
     public class OrganisationsController : Controller
     {
-        DatabaseController db = new DatabaseController("s");
-         
+        DatabaseController db = new DatabaseController("1");
+
         // GET: Organisation/Create
         public ActionResult Create()
         {
@@ -21,10 +21,32 @@ namespace Bil372_Odev1_Grup6.Controllers
 
         // POST: Organisation/Create
         [HttpPost]
-        public ActionResult Create(string oname,string pw, string oaddress, string ocity, string odist, string otype)
+        public ActionResult Create(string oname, string pw, string oaddress, string ocity, string odist, string otype)
         {
-            
-            db.insertOrganisations(oname, Int32.Parse(pw), true ,oaddress , Int32.Parse(ocity), odist,Int32.Parse(otype));
+            List<ORGANISATIONS> o = db.getOrganisations();
+            if (o.Count == 0)
+            {
+                db.insertOrganisations(oname, Int32.Parse(pw), false, oaddress, Int32.Parse(ocity), odist, Int32.Parse(otype));
+
+            }
+            else
+            {
+                bool check = false;
+                foreach (var org in o)
+                {
+                    if (org.PARENT_ORG == Int32.Parse(pw))
+                    {
+                        db.insertOrganisations(oname, Int32.Parse(pw), false, oaddress, Int32.Parse(ocity), odist, Int32.Parse(otype));
+                        db.updateOrganisations(org.ORG_ID, org.ORG_NAME, org.PARENT_ORG, true, org.ORG_ADDRESS, org.ORG_CITY, org.ORG_DISTRICT, org.ORG_TYPE);
+                        check = true;
+                    }
+                }
+                if (!check)
+                {
+                    db.insertOrganisations(oname, Int32.Parse(pw), false, oaddress, Int32.Parse(ocity), odist, Int32.Parse(otype));
+
+                }
+            }
             return View();
 
         }
@@ -47,21 +69,61 @@ namespace Bil372_Odev1_Grup6.Controllers
 
         // POST: Product/Update
         [HttpPost]
-        public ActionResult Update(string oid,string oname,string pid,string oaddress,string ocity,string odist, string otype)
+        public ActionResult Update(string oid, string oname, string pid, string oaddress, string ocity, string odist, string otype)
         {
-            db.updateOrganisations(Int32.Parse(oid),oname,Int32.Parse(pid),true,oaddress,Int32.Parse(ocity),odist,Int32.Parse(otype));
+            List<ORGANISATIONS> o = db.getOrganisations();
+            bool isAbstract = false;
+
+            foreach (var org in o)
+            {
+                if (org.ORG_ID == Int32.Parse(oid))
+                {
+                    isAbstract = org.ORG_ABSTRACT;
+                }
+            }
+            db.updateOrganisations(Int32.Parse(oid), oname, Int32.Parse(pid), isAbstract, oaddress, Int32.Parse(ocity), odist, Int32.Parse(otype));
             return View();
 
         }
 
 
         // GET: Organisation/Delete
-        public ActionResult Delete(string oid,string value)
+        public ActionResult Delete(string oid, string value)
         {
+            List<ORGANISATIONS> o = db.getOrganisations();
+
             if (oid != null)
+            {
                 db.deleteFromOrganisations(Int32.Parse(oid), Int32.Parse(value));
-            List<ORGANISATIONS> organisations = db.getOrganisations();
-            return View(organisations);
+
+                foreach (var org in o)
+                {
+                    bool check = false;
+                    if (Int32.Parse(oid) == org.ORG_ID)
+                    {
+                        int id = org.ORG_ID;
+                        foreach (var org2 in o)
+                        {
+                            if (org2.PARENT_ORG == id)
+                            {
+                                check = true;
+                            }
+                        }
+                        if (!check)
+                        {
+                            foreach (var parent in o)
+                            {
+                                if (org.PARENT_ORG == parent.ORG_ID)
+                                {
+                                    db.updateOrganisations(parent.ORG_ID, parent.ORG_NAME, parent.PARENT_ORG, false, parent.ORG_ADDRESS, parent.ORG_CITY, parent.ORG_DISTRICT, parent.ORG_TYPE);
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return View(o);
         }
 
 
